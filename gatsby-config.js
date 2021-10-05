3d1ad75dc281834e65692c20af8b9d2e3f775b0c
liyang <7325560@qq.com>
2021-10-05 08:30:07 +0800

    add prefix path config, update build depoly config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "D2 Info",
   },
+  pathPrefix: `/d2-info`,
   plugins: [
     `gatsby-transformer-json`,
     `gatsby-plugin-material-ui`,
